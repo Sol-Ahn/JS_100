ec74e381d6acffd2ea972162c2c5a6f4c9de2404
Sol-Ahn <sorry0194@gmail.com>
2021-01-11 18:17:24 +0900

    Add: Quiz 26-30

--- 21-30.js
+++ 21-30.js
@@ -1,5 +1,5 @@
 // 문제21: set은 어떻게 만드나요?
-// 다음 중 set을 만드는 방법으로 올바른 것을 모두 고르시오.
+// 다음 중 set을 만드는 방법으로 올바른 것을 모두 고르세요.
 
 /*선택지
 1) var x = {1, 2, 3, 5, 6, 7};
@@ -67,14 +67,14 @@
 
 // 내가 쓴 답안
 {
-  const name = prompt("이름을 입력하세요.");
-  console.log(name.toUpperCase());
+  //  const name = prompt("이름을 입력하세요.");
+  //  console.log(name.toUpperCase());
 }
 
 // 정답
 {
-  var name = prompt("이름을 입력하세요.");
-  console.log(name.toUpperCase());
+  //  var name = prompt("이름을 입력하세요.");
+  //  console.log(name.toUpperCase());
 }
 
 // --------------------------------------------------------------------------------------------
@@ -101,4 +101,210 @@
 
   const r = prompt("원의 반지름을 입력하세요.");
   console.log(circle(r));
+}
+
+// --------------------------------------------------------------------------------------------
+
+// 문제26: 행성 문제2
+// 우리 태양계를 이루는 행성은 수성, 금성, 지구, 화성, 목성, 토성, 천왕성, 해왕성이 있습니다.
+// 이 행성들의 영어 이름은 Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune입니다.
+// 행성의 한글 이름을 입력하면 영어 이름을 반환하는 프로그램을 만들어주세요.
+
+// 내가 쓴 답안1 - 함수
+{
+  const planet = prompt("행성의 이름을 입력하세요.");
+  const planetEng = (name) => {
+    if (name === "수성") return "Mercury";
+    else if (name === "금성") return "Venus";
+    else if (name === "지구") return "Earth";
+    else if (name === "화성") return "Mars";
+    else if (name === "목성") return "Jupiter";
+    else if (name === "토성") return "Saturn";
+    else if (name === "천왕성") return "Uranus";
+    else if (name === "해왕성") return "Neptune";
+  };
+  console.log(planetEng(planet));
+}
+
+// 내가 쓴 답안2 - switch문
+{
+  const planet = prompt("행성의 이름을 입력하세요.");
+  switch (planet) {
+    case "수성":
+      console.log("Mercury");
+      break;
+    case "금성":
+      console.log("Venus");
+      break;
+    case "지구":
+      console.log("Earth");
+      break;
+    case "화성":
+      console.log("Mars");
+      break;
+    case "목성":
+      console.log("Jupiter");
+      break;
+    case "토성":
+      console.log("Saturn");
+      break;
+    case "천왕성":
+      console.log("Uranus");
+      break;
+    case "해왕성":
+      console.log("Neptune");
+      break;
+    default:
+      alert("잘못된 입력입니다.");
+  }
+}
+
+// 정답
+{
+  const planets = {
+    수성: "Mercury",
+    금성: "Venus",
+    지구: "Earth",
+    화성: "Mars",
+    목성: "Jupiter",
+    토성: "Saturn",
+    천왕성: "Uranus",
+    해왕성: "Neptune",
+  };
+
+  const name = prompt("행성의 이름을 입력하세요.");
+
+  console.log(planets[name]);
+}
+
+// --------------------------------------------------------------------------------------------
+
+// 문제27: 객체 만들기
+// 첫번째 입력에서는 학생의 이름이 공백으로 구분되어 입력되고, 두번째에는 그 학생의 수학 점수가 공백으로 구분되어 주어집니다.
+// 두 개를 합쳐 학생의 이름이 key이고 value가 수학 점수인 객체를 출력해주세요.
+
+// 입력
+// Yujin Hyewon
+// 70 100
+
+// 출력
+// {'Yujin': 70, 'Hyewon': 100}
+
+// 내가 쓴 답안
+{
+  const name = prompt("학생 이름").split(" "); // 배열
+  const score = prompt("수학 점수").split(" "); // 배열
+
+  let result = {}; // const로 선언해야 함!
+  for (let i = 0; i < score.length; i++) {
+    result[name[i]] = parseInt(score[i], 10);
+  }
+  console.log(result);
+}
+
+// 정답
+{
+  const keys = prompt("이름을 입력하세요").split(" ");
+  const values = prompt("점수를 입력하세요").split(" ");
+  const obj = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    obj[keys[i]] = parseInt(values[i], 10);
+  }
+
+  console.log(obj);
+}
+
+// --------------------------------------------------------------------------------------------
+
+// 문제28: 2-gram
+// 2-gram이란 문자열에서 2개의 연속된 요소를 출력하는 방법입니다.
+// 예를 들어 'Javascript'를 2-gram으로 반복해 본다면 다음과 같은 결과가 나옵니다.
+
+// - 입력
+// Javascript
+// - 출력
+/*
+J a
+a v
+v a
+a s
+s c
+c r
+r i
+i p
+p t
+*/
+
+// 입력으로 문자열이 주어지면 2-gram으로 출력하는 프로그램을 작성해주세요.
+
+// 내가 쓴 답안
+{
+  const string = prompt("문자를 입력하세요.");
+
+  for (let i = 0; i < string.length - 1; i++) {
+    console.log(string[i], string[i + 1]);
+  }
+}
+
+// 정답
+{
+  const data = prompt("문자를 입력하세요.");
+
+  for (let i = 0; i < data.length - 1; i++) {
+    console.log(data[i], data[i + 1]);
+  }
+}
+
+// --------------------------------------------------------------------------------------------
+
+// 문제29: 대문자만 지나가세요
+// 알파벳 하나만을 입력하고 그 알파벳이 대문자이면 YES를 아니면 NO를 출력하는 프로그램을 만들어주세요.
+
+// 내가 쓴 답안
+{
+  const alphabet = prompt("알파벳을 입력하세요");
+  if (alphabet === alphabet.toUpperCase()) console.log("YES");
+  else console.log("NO");
+}
+
+// 정답
+{
+  const data = prompt("알파벳을 입력하세요.");
+  if (data === data.toUpperCase()) {
+    console.log("YES");
+  } else {
+    console.log("NO");
+  }
+}
+
+// --------------------------------------------------------------------------------------------
+
+// 문제30: 문자열 속 문자 찾기
+// 첫번째 입력에서는 문자열이 입력되고, 두번째에는 찾을 문자가 입력되어야 합니다.
+// 그 문자가 시작하는 index를 반환하는 프로그램을 만들어주세요.
+
+// - 입력
+// pineapple is yummy
+// apple
+
+// - 출력
+// 4
+
+// 내가 쓴 답안
+{
+  const string = prompt("문자열을 입력하세요.");
+  const word = prompt("문자를 입력하세요.");
+
+  const index = string.indexOf(word);
+  console.log(index);
+}
+
+// 정답
+{
+  const data = prompt("문자열을 입력하세요");
+  const word = prompt("찾을 단어를 입력하세요.");
+
+  console.log(data.indexOf(word));
+  // indexOf() 메서드는 호출한 String 객체나 배열에서 주어진 값과 일치하는 값 혹은 요소의 첫 번째 인덱스를 반환하고, 존재하지 않으면 -1을 반환합니다.
 }
