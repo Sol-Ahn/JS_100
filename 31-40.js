dfb4b9dcd1ca406653f6f47eb5e99ff10622dacb
Sol-Ahn <sorry0194@gmail.com>
2021-01-12 21:15:51 +0900

    Add: Quiz 33-35

--- 31-40.js
+++ 31-40.js
@@ -55,3 +55,105 @@
 }
 
 // -----------------------------------------------------------------------------------
+
+// 문제33: 거꾸로 출력하기
+// 한 줄에 여러개의 숫자가 입력되면, 역순으로 그 숫자들을 하나씩 출력하는 프로그램을 작성하시오.
+
+// 내가 쓴 답안
+{
+  const nums = prompt("숫자들을 입력하세요.").split(" ");
+  const result = nums.reverse().toString();
+  console.log(result.replaceAll(",", " "));
+}
+
+// 정답
+{
+  const data = prompt("숫자를 입력하세요.").split(" ").reverse();
+  let result = "";
+
+  for (let i = 0; i < data.length; i++) {
+    result += data[i];
+  }
+
+  console.log(result);
+}
+
+// -----------------------------------------------------------------------------------
+
+// 문제34: sort 구현하기
+// 키가 주어지면 순서대로 제대로 섰는지 확인하는 프로그램을 작성해보세요.
+// (키는 공백으로 구분하여 입력됩니다.)
+
+// 내가 쓴 답안
+{
+  const heights = prompt("키를 입력하세요.").split(" ");
+
+  function compareFunction(a, b) {
+    return a - b;
+  }
+
+  if (heights.toString() === heights.sort(compareFunction).toString())
+    console.log("YES");
+  else console.log("NO");
+}
+
+// 정답
+{
+  const unsorted = prompt("키를 입력하세요");
+  let sorted = "";
+
+  sorted = unsorted
+    .split(" ")
+    .sort(function (a, b) {
+      return a - b;
+    })
+    .join(" ");
+
+  if (unsorted === sorted) {
+    console.log("YES");
+  } else {
+    console.log("NO");
+  }
+}
+
+// -----------------------------------------------------------------------------------
+
+// 문제35: Factory 함수 사용하기
+// 2제곱, 3제곱, 4제곱을 할 수 있는 Factory 함수를 만들어보세요.
+
+// 내가 쓴 답안
+{
+  // two()에 들어가는 파라미터가 base value가 되고, onde()에 들어가는 파라미터가 exponent value가 된다.
+  function one(n) {
+    function two() {
+      result = Math.pow(n, n);
+      return result;
+    }
+    return two;
+  }
+  const a = one(2);
+  const b = one(3);
+  const c = one(4);
+  console.log(a(10));
+  console.log(b(10));
+  console.log(c(10));
+}
+
+// 정답
+{
+  function one(n) {
+    function two(value) {
+      const sq = Math.pow(value, n);
+      return sq;
+    }
+    return two;
+  }
+
+  const a = one(2);
+  const b = one(3);
+  const c = one(4);
+
+  console.log(a(10)); // 실질적으로 a(10)는 two(10)를 의미함.
+  console.log(b(10));
+  console.log(c(10));
+}
