6038933c3edefcaf3aad4f28e3db0a49ae76b775
Sol-Ahn <sorry0194@gmail.com>
2021-01-20 22:35:26 +0900

    Add: Quiz 47

--- 41-50.js
+++ 41-50.js
@@ -218,3 +218,36 @@
 
   console.log(sum); // 102
 }
+
+// -----------------------------------------------------------
+
+// 문제47: set 자료형의 응용
+// 아래 주어진 데이터들로부터 중복을 제거하여 실제 접수 인원을 출력해 주세요.
+const people = {
+  이호준: "01050442903",
+  이호상: "01051442904",
+  이준호: "01050342904",
+  이호준: "01050442903",
+  이준: "01050412904",
+  이호: "01050443904",
+  이호준: "01050442903",
+};
+
+// 내가 쓴 답안
+{
+  let set = new Set();
+  for (person in people) {
+    set.add(person); // key값을 담음
+  }
+
+  console.log(set.size);
+}
+
+// 정답
+{
+  let result = new Set();
+  for (let key in people) {
+    result.add(people[key]); // value값을 담음
+  }
+  console.log(result.size); // size는 set 객체에 있는 값의 개수를 반환
+}
